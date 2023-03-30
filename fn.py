5b5ec66cbeaa8dfefd4a4da7306ee436fa23f018
JoseAcun <josesanti09@hotmail.com>
2023-03-30 14:19:51 -0500

    inset persona

--- fn.py
+++ fn.py
@@ -38,23 +38,59 @@ def insert_email():
     cur.execute(sql, (correo, dominio))
     con.commit()
     # funcion para agregar datos en la tabla personas
+
+
 def insert_persona():
     con = sqlite3.connect("Fincapp.db")
 
     cur = con.cursor()  
+    # se consultan los ultimos id generados de las tablas que contienen las llaves primarias respectivas a las foraneas de persona
+    result = con.execute("SELECT id FROM telefono ORDER BY id DESC LIMIT 1")
+    last_id_telefono = result.fetchone()[0]
 
-    tipo_persona = input
+    result = con.execute("SELECT id FROM email ORDER BY id DESC LIMIT 1")
+    last_id_email = result.fetchone()[0]
+
+    nombre_completo = input("Introduce el nombre completo de la persona: ")
+
+    tipo_persona = int(input("""Seleccione el tipo de persona: \n
+                            1.Natural \n
+                            2.Juridica \n"""))
+
+    nombres_apellidos = nombre_completo.split()
+    nombres = nombres_apellidos[:2]
+    apellidos = nombres_apellidos[2:]
+
+    if len(nombres) == 1:
+        nombre_1 = nombres[0]
+        nombre_2 = ''
+    else:
+        nombre_1 = nombres[0]
+        nombre_2 = nombres[1]
+
+    if len(apellidos) == 1:
+        apellido_1 = apellidos[0]
+        apellido_2 = ''
+    else:
+        apellido_1 = apellidos[0]
+        apellido_2 = apellidos[1]
+
+        con.execute("INSERT INTO persona (nom1, nom2, ap1, ap2, id_email, id_telefono, id_tipo_persona) VALUES (?, ?, ?, ?, ?, ?, ?)", 
+                    (nombre_1, nombre_2, apellido_1, apellido_2, last_id_email, last_id_telefono, tipo_persona))
 
     con.commit()
 
-
+# insert_email()
+# insert_telefono()
+# insert_persona()
 con = sqlite3.connect("Fincapp.db")
 cur = con.cursor()
 
-cur.execute("select * from email")
+cur.execute("""select p.nom1, p.ap1, t.numero,e.correo || '@' || e.dominio as correo, tp.tipo_persona from persona p 
+                 inner join telefono t on t.id = p.id_telefono 
+                 inner join email e on e.id = p.id_email
+                 inner join tipo_persona tp on tp.id = p.id_tipo_persona""")
+
+
 print(cur.fetchall())
-# data = [
-#     ("Monty", "Python", "Live at the," "Hollywood Bowl", 1982, 7.9)
-# ]
-# cur.executemany("INSERT INTO persona VALUES(?, ?, ?)", data)
 con.commit()
