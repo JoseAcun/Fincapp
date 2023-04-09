eb1fa9339bccc20a6026f79ec5f874db6bfe5869
Wilfrank Alvarez <Welolmos12345@gmail.com>
2023-04-09 14:34:06 -0500

    Front - backend (Login)
    se agregó la funcionalidad del registro y login

---
+++ dbmain.py
@@ -0,0 +1,227 @@
+import sqlite3
+
+
+class DbMain():
+    def __init__(self, dbname):
+        self.dbname = dbname
+        self.conn = sqlite3.connect(self.dbname)
+        self.curs = self.conn.cursor()
+
+        cmd = """CREATE TABLE IF NOT EXISTS email(id integer primary key autoincrement,correo varchar(50),dominio varchar(50))"""
+        self.curs.execute(cmd)
+        self.conn.commit()
+
+        cmd = """CREATE TABLE IF NOT EXISTS telefono(id integer primary key autoincrement,prefijo int,numero bigint)"""
+        self.curs.execute(cmd)
+        self.conn.commit()
+        
+        cmd = """CREATE TABLE IF NOT EXISTS tipo_persona(id integer primary key autoincrement, tipo_persona varchar(25))"""
+        self.curs.execute(cmd)
+        self.conn.commit()
+        
+        cmd = """CREATE TABLE IF NOT EXISTS persona(id integer primary key autoincrement,nom1 varchar(50),nom2 varchar(50),ap1 varchar(50),ap2 varchar(50),id_email int,id_telefono int, id_tipo_persona int, active INTEGER NOT NULL, foreign key (id_tipo_persona) references tipo_persona(id), foreign key (id_email) references email(id),foreign key (id_telefono) references telefono(id))"""
+        self.curs.execute(cmd)
+        self.conn.commit()
+        
+        cmd = """CREATE TABLE IF NOT EXISTS trabajador(id integer primary key autoincrement,sueldo float,id_persona int,foreign key (id_persona) references persona(id))"""
+        self.curs.execute(cmd)
+        self.conn.commit()
+        
+        cmd = """CREATE TABLE IF NOT EXISTS metodopago(id integer primary key autoincrement,metodo_pago varchar(50))"""
+        self.curs.execute(cmd)
+        self.conn.commit()
+        
+        cmd = """CREATE TABLE IF NOT EXISTS pago(id integer primary key autoincrement,id_metodopago int,foreign key (id_metodopago) references metodopago(id))"""
+        self.curs.execute(cmd)
+        self.conn.commit()
+        
+        cmd = """CREATE TABLE IF NOT EXISTS categoria(id integer primary key autoincrement,nom_cat varchar(50),icono blob)"""
+        self.curs.execute(cmd)
+        self.conn.commit()
+        
+        cmd = """CREATE TABLE IF NOT EXISTS producto(id integer primary key autoincrement,nombre varchar(50),valor_unit float,descripcion text,id_categoria int,foreign key (id_categoria) references categoria(id))"""
+        self.curs.execute(cmd)
+        self.conn.commit()
+        
+        cmd = """CREATE TABLE IF NOT EXISTS administrador(id integer primary key autoincrement,usuario varchar(25),passwordu varchar(25),id_persona int,foreign key (id_persona) references persona(id))"""
+        self.curs.execute(cmd)
+        self.conn.commit()
+        
+        cmd = """CREATE TABLE IF NOT EXISTS movimiento(cantidad int,fecha date,descripcion_movimiento text,id_persona int,id_pago int,id_producto int,foreign key (id_producto) references producto(id),foreign key (id_pago) references pago(id),foreign key (id_persona) references persona(id))"""
+        self.curs.execute(cmd)
+        self.conn.commit()
+        
+        cmd = """CREATE TABLE IF NOT EXISTS curr_admin(currid INTEGER, active INTEGER NOT NULL)"""
+        self.curs.execute(cmd)
+        self.conn.commit()
+        
+        cmd = """INSERT INTO tipo_persona(tipo_persona) values('Natural'),('Juridica')"""
+        self.curs.execute(cmd)
+        self.conn.commit()
+        
+        cmd = """INSERT INTO metodopago(metodo_pago) values('Efectivo'), ('tarjeta'), ('transferencia'), ('otro')"""
+        self.curs.execute(cmd)
+        self.conn.commit()
+        self.conn.close()
+        
+        self.trc_tbl('curr_admin')
+
+    def ins_tbl_telefono(self, indice, numero):
+        self.conn = sqlite3.connect(self.dbname)
+        self.curs = self.conn.cursor()
+        sqltext = """INSERT INTO telefono(prefijo, numero)
+                    VALUES(?,?)"""
+        data_tuple = (indice, numero)
+        self.curs.execute(sqltext, data_tuple)
+        self.conn.commit()
+        self.conn.close()
+
+    def ins_tbl_email(self, correof):
+        self.conn = sqlite3.connect(self.dbname)
+        self.curs = self.conn.cursor()
+        separador = "@"
+        correoS = correof.split(separador)
+        correo = correoS[0]  
+        dominio = correoS[1]  
+              
+        sqltext = """INSERT INTO email(correo, dominio)
+                    VALUES(UPPER(?), UPPER(?))"""
+        data_tuple = (correo, dominio)
+        self.curs.execute(sqltext, data_tuple)
+        self.conn.commit()
+        self.conn.close()
+
+    def ins_tbl_persona(self, usuario, nombre_completo, passwordu, tipo_persona, active):
+        self.conn = sqlite3.connect(self.dbname)
+        self.curs = self.conn.cursor()
+        
+        result = self.conn.execute("SELECT id FROM telefono ORDER BY id DESC LIMIT 1")
+        last_id_telefono = result.fetchone()[0]
+
+        result = self.conn.execute("SELECT id FROM email ORDER BY id DESC LIMIT 1")
+        last_id_email = result.fetchone()[0]
+
+        nombres_apellidos = nombre_completo.split()
+        nombres = nombres_apellidos[:2]
+        apellidos = nombres_apellidos[2:]
+
+        if len(nombres) == 1:
+            nombre_1 = nombres[0]
+            nombre_2 = ''
+        else:
+            nombre_1 = nombres[0]
+            nombre_2 = nombres[1]
+
+        if len(apellidos) == 1:
+            apellido_1 = apellidos[0]
+            apellido_2 = ''
+        else:
+            apellido_1 = apellidos[0]
+            apellido_2 = apellidos[1]
+        
+        sqltext = """INSERT INTO persona(nom1, nom2, ap1, ap2, id_email, id_telefono, id_tipo_persona, active)
+                    VALUES(?,?,?,?,?,?,?,?)"""
+        
+        data_tuple = (nombre_1, nombre_2, apellido_1, apellido_2, last_id_email, last_id_telefono, tipo_persona, active)
+        self.curs.execute(sqltext, data_tuple)
+           
+        result = self.conn.execute("SELECT id FROM persona ORDER BY id DESC LIMIT 1")
+        last_id_persona = result.fetchone()[0]
+                    
+        sqltexta = """INSERT INTO administrador(usuario, passwordu, id_persona)
+                    VALUES(?,?,?)"""
+        data_tuplea = (usuario, passwordu, last_id_persona)
+        self.curs.execute(sqltexta, data_tuplea)
+        self.conn.commit()
+        self.conn.close()
+
+    def chk_usuario_cont(self, usuario, passwordu):
+        self.conn = sqlite3.connect(self.dbname)
+        self.curs = self.conn.cursor()
+        sqltext = "SELECT EXISTS(SELECT 1 FROM administrador WHERE usuario='{}' AND passwordu='{}' LIMIT 1)".format(
+            usuario, passwordu)
+        self.curs.execute(sqltext)
+        row = self.curs.fetchone()
+        self.conn.close()
+        return row[0]
+
+    def chk_exist_user(self, usuario):
+        self.conn = sqlite3.connect(self.dbname)
+        self.curs = self.conn.cursor()
+        sqltext = "SELECT EXISTS(SELECT 1 FROM administrador WHERE usuario='{}' LIMIT 1)".format(
+            usuario)
+        self.curs.execute(sqltext)
+        row = self.curs.fetchone()
+        self.conn.close()
+        return row[0]
+
+    def get_user_id(self, usuario):
+        self.conn = sqlite3.connect(self.dbname)
+        self.curs = self.conn.cursor()
+        sqltext = "SELECT id FROM administrador WHERE usuario='{}' LIMIT 1".format(
+            usuario)
+        self.curs.execute(sqltext)
+        row = self.curs.fetchone()
+        self.conn.close()
+        return row[0]
+
+    def get_user_name(self, id):
+        self.conn = sqlite3.connect(self.dbname)
+        self.curs = self.conn.cursor()
+        sqltext = "SELECT nom1 FROM persona WHERE id='{}' LIMIT 1".format(id)
+        self.curs.execute(sqltext)
+        row = self.curs.fetchone()
+        self.conn.close()
+        if row is not None:
+            return row[0]
+        else:
+            return None
+
+    def get_user_email(self, id):
+        self.conn = sqlite3.connect(self.dbname)
+        self.curs = self.conn.cursor()
+        sqltext = "SELECT correo FROM email WHERE id='{}' LIMIT 1".format(
+            id)
+        self.curs.execute(sqltext)
+        row = self.curs.fetchone()
+        self.conn.close()
+        return row[0]
+
+    def get_user_pswd(self, id):
+        self.conn = sqlite3.connect(self.dbname)
+        self.curs = self.conn.cursor()
+        sqltext = "SELECT password FROM administrador WHERE id='{}' LIMIT 1".format(
+            id)
+        self.curs.execute(sqltext)
+        row = self.curs.fetchone()
+        self.conn.close()
+        return row[0]
+
+    def upd_curr_user(self, id):
+        self.conn = sqlite3.connect(self.dbname)
+        self.curs = self.conn.cursor()
+        sqltext = """INSERT INTO curr_admin(currid,active)
+                    VALUES(?,?)"""
+        data_tuple = (id, 1)
+        self.curs.execute(sqltext, data_tuple)
+        self.conn.commit()
+        self.conn.close()
+
+    def upd_user_passwd(self, id, nupwd):
+        self.conn = sqlite3.connect(self.dbname)
+        self.curs = self.conn.cursor()
+        sqltext = """UPDATE administrador SET password = '{}' WHERE id = {}""".format(
+            nupwd, id)
+        print(sqltext)
+        self.curs.execute(sqltext)
+        self.conn.commit()
+        self.conn.close()
+
+    def trc_tbl(self, tbl_name):
+        self.conn = sqlite3.connect(self.dbname)
+        self.curs = self.conn.cursor()
+        sqltext = "DELETE FROM "+tbl_name
+        self.curs.execute(sqltext)
+        self.conn.commit()
+        self.conn.close()
+        # return 1
--- main.py
+++ main.py
@@ -1,420 +1,105 @@
-import tkinter as tk
-from tkinter import ttk
-
-# Crea una instancia de la ventana principal
-root = tk.Tk()
-
-# Establece el título de la ventana
-root.title("Mi aplicación")
-
-# Establece las dimensiones de la ventana
-root.geometry("600x400")
-
-# Función para mostrar la página de inicio de sesión
-def show_login():
-    
-    # Elimina cualquier widget que esté en la ventana principal
-    for widget in root.winfo_children():
-        widget.destroy()
-
-    # Crea los widgets necesarios para la página de inicio de sesión
-    label_username = tk.Label(root, text="Usuario")
-    label_username.pack()
-
-    entry_username = tk.Entry(root)
-    entry_username.pack()
-
-    label_password = tk.Label(root, text="Contraseña")
-    label_password.pack()
-
-    entry_password = tk.Entry(root, show="*")
-    entry_password.pack()
-
-    button_login = tk.Button(root, text="Iniciar sesión", command=show_movements)
-    button_login.pack()
-    
-    button_login = tk.Button(root, text="Registrarse", command=show_register)
-    button_login.pack()
-
-# Función para mostrar la página de registro
-def show_register():
-    # Elimina cualquier widget que esté en la ventana principal
-    for widget in root.winfo_children():
-        widget.destroy()
-
-    # Crea los widgets necesarios para la página de registro
-    label_username = tk.Label(root, text="Usuario")
-    label_username.pack()
-
-    entry_username = tk.Entry(root)
-    entry_username.pack()
-
-    label_password = tk.Label(root, text="Contraseña")
-    label_password.pack()
-
-    entry_password = tk.Entry(root, show="*")
-    entry_password.pack()
-
-    button_register = tk.Button(root, text="Registrarse", command=show_movements)
-    button_register.pack()
-
-# Función para mostrar la tabla de movimientos
-def show_movements():
-    # Elimina cualquier widget que esté en la ventana principal
-    for widget in root.winfo_children():
-        widget.destroy()
-
-    # Menu desplegable
-    menu = tk.Menu()
-
-    # Crea los widgets necesarios para la tabla de movimientos
-    label_movements = tk.Label(root, text="Movimientos")
-    label_movements.pack()
-
-    treeview = ttk.Treeview(root, columns=("Fecha", "Concepto", "Cantidad"))
-    treeview.heading("#0", text="ID")
-    treeview.heading("Fecha", text="Fecha")
-    treeview.heading("Concepto", text="Concepto")
-    treeview.heading("Cantidad", text="Cantidad")
-    treeview.pack()
-
-    # Agrega algunos datos de muestra a la tabla
-    for i in range(10):
-        treeview.insert("", "end", text=i, values=("2022-04-01", "Concepto {}".format(i), "${}".format(i*100)))
-
-    button_profile = tk.Button(root, text="Perfil", command=show_profile)
-    button_profile.pack(side=tk.TOP)
-
-    button_newmov = tk.Button(root, text="Nuevo Movimiento", command=show_newmov)
-    button_newmov.pack(side=tk.TOP)
-    
-    button_vercats = tk.Button(root, text="Ver Categorias", command=show_Cats)
-    button_vercats.pack(side=tk.TOP)
-    
-    button_verprods = tk.Button(root, text="Ver Productos", command=show_prods)
-    button_verprods.pack(side=tk.TOP)
-    
-    button_vertrab = tk.Button(root, text="Ver Trabajadores", command=show_trabajadores)
-    button_vertrab.pack(side=tk.TOP)
-
-def show_Cats():
-    # Elimina cualquier widget que esté en la ventana principal
-    for widget in root.winfo_children():
-        widget.destroy()
-
-    label_categorias = tk.Label(root, text="Categorias")
-    label_categorias.pack()
-
-    treeview = ttk.Treeview(root, columns=("Fecha", "Concepto", "Cantidad"))
-    treeview.heading("#0", text="ID")
-    treeview.heading("Fecha", text="Fecha")
-    treeview.heading("Concepto", text="Concepto")
-    treeview.heading("Cantidad", text="Cantidad")
-    treeview.pack()
-
-    for i in range(10):
-        treeview.insert("", "end", text=i, values=("2022-04-01", "Concepto {}".format(i), "${}".format(i*100)))
-
-    button_inscategorias = tk.Button(root, text="Crear Categoría", command=show_inscategoria)
-    button_inscategorias.pack()
-
-    button_volvcat = tk.Button(root, text="Volver", command=show_movements)
-    button_volvcat.pack()
-
-def show_prods():
-    # Elimina cualquier widget que esté en la ventana principal
-    for widget in root.winfo_children():
-        widget.destroy()
-
-    label_productos = tk.Label(root, text="Productos")
-    label_productos.pack()
-
-    treeview = ttk.Treeview(root, columns=("Fecha", "Concepto", "Cantidad"))
-    treeview.heading("#0", text="ID")
-    treeview.heading("Fecha", text="Fecha")
-    treeview.heading("Concepto", text="Concepto")
-    treeview.heading("Cantidad", text="Cantidad")
-    treeview.pack()
-
-    # Agrega algunos datos de muestra a la tabla
-    for i in range(10):
-        treeview.insert("", "end", text=i, values=("2022-04-01", "Concepto {}".format(i), "${}".format(i*100)))
-
-    button_insproductos = tk.Button(root, text="Ingresar Productos", command=show_insprods)
-    button_insproductos.pack()
+__version__ = "1.0"
+from kivymd.app import MDApp
+from kivy.core.window import Window
+from kivy.uix.screenmanager import Screen, NoTransition, CardTransition
+from dbmain import DbMain
+from kivy.properties import ObjectProperty
+from kivymd.uix.snackbar import Snackbar
+
+
+Window.size = (375, 750)
+
+
+class HomeScreen(Screen):
+    pass
+
+
+class LoginScreen(Screen):
+    pass
+
+
+class RegisterScreen(Screen):
+    pass
+
+
+class SettingsScreen(Screen):
+    pass
+
+
+class MainApp(MDApp):
+
+    def on_start(self):
+        self.theme_cls.primary_palette = 'Purple'
+        self.theme_cls.primaru_hue = '500'
+        self.theme_cls.theme_style = 'Dark'
+        self.db = DbMain('myappdb.db')
+        self.curr_user_id = -1
+        self.curr_user_name = ""
+        self.curr_user_email = ""
+
+    def change_screen(self, screen_name, direction, mode):
+        screen_manager = self.root.ids.screen_manager
+        if direction == "None":
+            screen_manager.transition = NoTransition()
+            screen_manager.current = screen_name
+            return
+        screen_manager.transition = CardTransition(
+            direction=direction, mode=mode)
+        screen_manager.current = screen_name
+
+    def opensidebarmenu(self, action):
+        self.root.ids.nav_drawer.set_state(action)
+
+    def chk_user(self, uname, upswd):
+        screen_manager = self.root.ids.screen_manager
+        chk_authorized = self.db.chk_usuario_cont(uname, upswd)
+
+        return_str = self.db.get_user_name(3)
+        print(return_str)
+
+        if(chk_authorized == 1):
+            print("Authorized access!!")
+            self.change_screen("scr_home", direction='right', mode='push')
+            self.curr_user_id = self.db.get_user_id(uname)
+            self.curr_user_name = self.db.get_user_name(self.curr_user_id)
+            self.curr_user_email = self.db.get_user_email(self.curr_user_id)
+            self.root.ids['navsidebar_user_name'].text = self.curr_user_name
+            self.root.ids['navsidebar_user_email'].text = self.curr_user_email
+            self.db.upd_curr_user(self.curr_user_id)
+        else:
+            screen_manager.get_screen(
+                'scr_login').ids.txt_login_info.text = "[color=ff0000]Invalid Username or Password. Try again![/color]"
+            print("UNauthorized access!!")
+        print("hello! Ur name : {}, id : {}, password : {}?".format(
+            uname, self.curr_user_id, upswd))
+
+    def register_user(self, usuario, nombre_completo, correof, indice, numero, passwordu):
+        screen_manager = self.root.ids.screen_manager
+        if(self.db.chk_exist_user(usuario) == 1):
+            screen_manager.get_screen(
+                'scr_register').ids.txt_info_register.text = "[color=ff0000]Nombre de usuario ya existe, Escoje otro.[/color]"
+        else:
+            self.db.ins_tbl_telefono(indice, numero)
+            self.db.ins_tbl_email(correof)            
+            self.db.ins_tbl_persona(usuario, nombre_completo, passwordu, 1, 1)
+            self.change_screen("scr_login", direction='left', mode='push')
+            Snackbar(text="New User Registered!").open()
+            
+    def leftmenucallback(self, x):
+        screen_manager = self.root.ids.screen_manager
+        self.change_screen("scr_settings", direction='left', mode='push')
+        #Snackbar(text="Under Construction..").open()
+
+    def save_settings(self, old_pwd, new_pwd):
+        screen_manager = self.root.ids.screen_manager
+        curr_pwd = self.db.get_user_pswd(self.curr_user_id)
+        if(curr_pwd == old_pwd):
+            self.db.upd_user_passwd(self.curr_user_id, new_pwd)
+            self.change_screen("scr_login", direction='right', mode='push')
+            Snackbar(text="New Password Updated!").open()
+        else:
+            screen_manager.get_screen(
+                'scr_settings').ids.txt_info_settings.text = "[color=ff0000]Wrong old password.[/color]"
 
-    button_volvprods = tk.Button(root, text="Volver", command=show_movements)
-    button_volvprods.pack()
-
-def show_insprods():
-    
-    # Elimina cualquier widget que esté en la ventana principal
-    for widget in root.winfo_children():
-        widget.destroy()
-
-    # Crea los widgets necesarios para la página de inicio de sesión
-    label_insprods = tk.Label(root, text="Modulo Ingreso de Productos")
-    label_insprods.pack()
 
-    label_selcat = tk.Label(root, text="Seleccione la categoria del producto")
-    label_selcat.pack()
-    
-    variable = tk.StringVar(root)
-    
-    variable.set("Frutas")
-    
-    w = tk.OptionMenu(root, variable, "Frutas", "Verduras", "Lacteos")
-    w.pack()
-
-    label_nomprod = tk.Label(root, text="Nombre Producto")
-    label_nomprod.pack()
-
-    entry_nomprod = tk.Entry(root)
-    entry_nomprod.pack()
-
-    label_cantprod = tk.Label(root, text="Cantidad de producto")
-    label_cantprod.pack()
-
-    entry_cantprod = tk.Entry(root)
-    entry_cantprod.pack()
-    
-    label_precioprod = tk.Label(root, text="Precio unitario del producto")
-    label_precioprod.pack()
-
-    entry_precioprod = tk.Entry(root)
-    entry_precioprod.pack()
-
-    label_Descprod = tk.Label(root, text="Descripcion del producto")
-    label_Descprod.pack()
-
-    entry_Descprod = tk.Entry(root)
-    entry_Descprod.pack()
-
-    button_insprod = tk.Button(root, text="Ingresar producto", command=show_prods)
-    button_insprod.pack()
-
-    button_volvtrab = tk.Button(root, text="Volver", command=show_movements)
-    button_volvtrab.pack()
-
-def show_instrabajador():
-    
-    # Elimina cualquier widget que esté en la ventana principal
-    for widget in root.winfo_children():
-        widget.destroy()
-
-    # Crea los widgets necesarios para la página de inicio de sesión
-    label_instrab = tk.Label(root, text="Modulo Ingreso de Trabajadores")
-    label_instrab.pack()
-
-    label_nomtrab = tk.Label(root, text="Nombre del trabajador")
-    label_nomtrab.pack()
-
-    entry_nomtrab = tk.Entry(root)
-    entry_nomtrab.pack()
-
-    label_apetrab = tk.Label(root, text="Apellido del trabajador")
-    label_apetrab.pack()
-
-    entry_apeprod = tk.Entry(root)
-    entry_apeprod.pack()
-    
-    label_teltrab = tk.Label(root, text="Telefono")
-    label_teltrab.pack()
-
-    entry_teltrab = tk.Entry(root)
-    entry_teltrab.pack()
-
-    label_sueltrab = tk.Label(root, text="Sueldo")
-    label_sueltrab.pack()
-
-    entry_sueltrab = tk.Entry(root)
-    entry_sueltrab.pack()
-
-    button_insprod = tk.Button(root, text="Ingresar Trabajador", command=show_trabajadores)
-    button_insprod.pack()
-
-    button_volvtrab = tk.Button(root, text="Volver", command=show_trabajadores)
-    button_volvtrab.pack()
-
-def show_inscategoria():
-    
-    # Elimina cualquier widget que esté en la ventana principal
-    for widget in root.winfo_children():
-        widget.destroy()
-
-    # Crea los widgets necesarios para la página de inicio de sesión
-    label_inscat = tk.Label(root, text="Modulo Ingreso de categorias")
-    label_inscat.pack()
-
-    label_nomcat = tk.Label(root, text="Nombre de la categoria")
-    label_nomcat.pack()
-
-    entry_nomcat = tk.Entry(root)
-    entry_nomcat.pack()
-
-    label_icocat = tk.Label(root, text="Insertar Icono")
-    label_icocat.pack()
-
-    button_icocat = tk.Button(root, text="+")
-    button_icocat.pack()
-
-    button_insprod = tk.Button(root, text="Ingresar Categoria", command=show_Cats)
-    button_insprod.pack()
-
-    button_volvtrab = tk.Button(root, text="Volver", command=show_Cats)
-    button_volvtrab.pack()
-
-
-
-def show_trabajadores():
-
-    for widget in root.winfo_children():
-        widget.destroy()
-
-    label_trabajadores = tk.Label(root, text="Trabajadores")
-    label_trabajadores.pack()
-
-    treeview = ttk.Treeview(root, columns=("Fecha", "Concepto", "Cantidad"))
-    treeview.heading("#0", text="ID")
-    treeview.heading("Fecha", text="Fecha")
-    treeview.heading("Concepto", text="Concepto")
-    treeview.heading("Cantidad", text="Cantidad")
-    treeview.pack()
-
-    for i in range(10):
-        treeview.insert("", "end", text=i, values=("2022-04-01", "Concepto {}".format(i), "${}".format(i*100)))
-
-    button_instrabajadores = tk.Button(root, text="Nuevo Trabajador", command=show_instrabajador)
-    button_instrabajadores.pack()
-
-    button_volvtrab = tk.Button(root, text="Volver", command=show_movements)
-    button_volvtrab.pack()
-
-def show_profile():
-    # Elimina cualquier widget que esté en la ventana principal
-    for widget in root.winfo_children():
-        widget.destroy()
-
-    # Crea los widgets necesarios para la página de perfil
-    label_profile = tk.Label(root, text="Perfil")
-    label_profile.pack()
-
-    button_logout = tk.Button(root, text="Cerrar sesión", command=show_login)
-    button_logout.pack()
-
-def show_newmov():
-    
-    for widget in root.winfo_children():
-        widget.destroy()
-
-    label_typemov = tk.Label(root, text="Tipo de Movimiento")
-    label_typemov.pack()
-
-    button_insprod = tk.Button(root, text="Ingresar Producto", command=show_insprods)
-    button_insprod.pack()
-    
-    button_salprod = tk.Button(root, text="Salida por Venta", command=show_salprod)
-    button_salprod.pack()
-    
-    button_salprodo = tk.Button(root, text="Salida por Otro Motivo", command=show_salprodo)
-    button_salprodo.pack()
-
-def show_salprod():
-    
-    # Elimina cualquier widget que esté en la ventana principal
-    for widget in root.winfo_children():
-        widget.destroy()
-
-    # Crea los widgets necesarios para la página de inicio de sesión
-    label_salprod = tk.Label(root, text="Venta")
-    label_salprod.pack()
-
-    label_selprod = tk.Label(root, text="Seleccione el producto")
-    label_selprod.pack()
-    
-    variable = tk.StringVar(root)
-    
-    variable.set("Mango")
-    
-    w = tk.OptionMenu(root, variable, "Mango", "Limon", "Leche")
-    w.pack()
-
-    label_cantprod = tk.Label(root, text="Cantidad a vender")
-    label_cantprod.pack()
-
-    entry_cantprod = tk.Entry(root)
-    entry_cantprod.pack()
-    
-    label_Metopag = tk.Label(root, text="Seleccione Metodo de Pago")
-    label_Metopag.pack()
-    
-    variable1 = tk.StringVar(root)
-    
-    variable1.set("Efectivo")
-    
-    w = tk.OptionMenu(root, variable1, "Efectivo", "PSE", "Transferencia")
-    w.pack()
-
-    label_valvent = tk.Label(root, text="Valor total de la venta")
-    label_valvent.pack()
-
-    text_field = tk.Entry(root, state='disabled')
-    text_field.pack()   
-    
-    
-    button_Realvent = tk.Button(root, text="Realizar Venta", command=show_movements)
-    button_Realvent.pack()
-
-    button_Realvent = tk.Button(root, text="Volver", command=show_newmov)
-    button_Realvent.pack()
-
-def show_salprodo():
-    
-    # Elimina cualquier widget que esté en la ventana principal
-    for widget in root.winfo_children():
-        widget.destroy()
-
-    # Crea los widgets necesarios para la página de inicio de sesión
-    label_salprod = tk.Label(root, text="Otra Salida")
-    label_salprod.pack()
-
-    label_selprod = tk.Label(root, text="Seleccione el producto")
-    label_selprod.pack()
-    
-    variable = tk.StringVar(root)
-    
-    variable.set("Mango")
-    
-    w = tk.OptionMenu(root, variable, "Mango", "Limon", "Leche")
-    w.pack()
-
-    label_cantprod = tk.Label(root, text="Cantidad")
-    label_cantprod.pack()
-
-    entry_cantprod = tk.Entry(root)
-    entry_cantprod.pack()
-    
-    label_Metopag = tk.Label(root, text="Descripcion de Salida")
-    label_Metopag.pack()
-    
-    entry_descsal = tk.Entry(root)
-    entry_descsal.pack()
-
-    label_valvent = tk.Label(root, text="Valor Total de la Salida")
-    label_valvent.pack()
-
-    text_field = tk.Entry(root, state='disabled')
-    text_field.pack()   
-    
-    button_RealSal = tk.Button(root, text="Procesar Salida", command=show_movements)
-    button_RealSal.pack()
-
-    button_RealSal = tk.Button(root, text="Volver", command=show_newmov)
-    button_RealSal.pack()
-
-
-# Mostrar la página de inicio de sesión al inicio
-show_login()
-
-# Inicia el bucle principal de la ventana
-root.mainloop()
+MainApp().run()
